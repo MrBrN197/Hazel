3ede9ab3328e305c96a812e0159c2d1fa0dfcf87
Brian Orora <brianorora@gmail.com>
2019-08-27 00:36:25 +0300

    Fixed typo in LayerStatck

--- Hazel/src/Hazel/LayerStack.cpp
+++ Hazel/src/Hazel/LayerStack.cpp
@@ -10,23 +10,23 @@ namespace Hazel {
 	}
 	
 	void LayerStack::PushLayer(Layer* layer){
-		m_Layers.emplace(m_Layers.begin() + m_LayerInsterIndex, layer);
-		m_LayerInsterIndex++;
+		m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
+		m_LayerInsertIndex++;
 	}
 	void LayerStack::PushOverlay(Layer* overlay){
 		m_Layers.emplace_back(overlay);
 	}
 
 	void LayerStack::PopLayer(Layer* layer){
-		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsterIndex, layer);
+		auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
 		if (it != m_Layers.end())
 		{
 			m_Layers.erase(it);
-			m_LayerInsterIndex--;
+			m_LayerInsertIndex--;
 		}
 	}
 	void LayerStack::PopOverlay(Layer* overlay){
-		auto it = std::find(m_Layers.begin() + m_LayerInsterIndex, m_Layers.end(), overlay);
+		auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
 		if (it != m_Layers.end())
 			m_Layers.erase(it);
 
